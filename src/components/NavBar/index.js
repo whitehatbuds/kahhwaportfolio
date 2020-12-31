7e79a2a6febd2da3506b1c73e3275cc8e53dc78a
Kah Hwa <whitehatbuds@gmail.com>
2020-12-31 22:37:57 +0800

    Remove Redundancies in NavBar and Index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,16 +10,15 @@ const NavBar = () => {
     const collapseNavBar = () => setIsCollapse(false);
 
     useEffect(() => {
-        let watchScroll = () => window.addEventListener("scroll", isAtTopPosition);
-        watchScroll();
+        window.addEventListener('scroll', isAtTopPosition);
         return () => window.removeEventListener("scroll", isAtTopPosition);
     });
 
-    const isAtTopPosition = () => (window.pageYOffset === 0) ? setisAtTop(true) : setisAtTop(false);
+    const isAtTopPosition = () => (window.pageYOffset) ? setisAtTop(false) : setisAtTop(true);
     return (
         <NavBar1 id="NavBar" isAtTop={isAtTop}>
             <NavContainer1>
-                <NavLogo1 href='#top'>
+                <NavLogo1 href='#'>
                     Kah Hwa's <span> Website</span>
                 </NavLogo1>
                 <NavBarMenuContainer id="NavMenuButton" onClick={toggleClick} >
@@ -27,7 +26,6 @@ const NavBar = () => {
                 </NavBarMenuContainer>
                 <NavItemContainer id="NavMenu" isCollapse={isCollapse}>
                     <NavItem href='#About' onClick={collapseNavBar}>About</NavItem>
-                    <NavItem href='Skills1.svg' onClick={collapseNavBar}>About</NavItem>
                     <NavItem href='#Skills' onClick={collapseNavBar}>Skills</NavItem>
                     <NavItem href='#Contact' onClick={collapseNavBar}>Contact</NavItem>
                 </NavItemContainer>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './components/Main';
 ReactDOM.render(
-  <>
-    <Main />
-  </>,
+    <Main />,
   document.getElementById('root')
 );
